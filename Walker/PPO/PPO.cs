04f9ac766f1b8db094c6e8396cf630ad62773187
squareGITHUB <squarectrl@gmail.com>
2023-06-07 13:50:36 +0100

    add seperate file for testing

--- Walker/PPO/PPO.cs
+++ Walker/PPO/PPO.cs
@@ -4,7 +4,7 @@ using Physics.Walker.PPO.Network;
 
 namespace Physics.Walker.PPO;
 
-public class PPO
+public partial class PPO
 {
     private readonly NeuralNetwork _criticNetwork;
     private readonly NeuralNetwork _actorNetwork;
@@ -176,62 +176,5 @@ public class PPO
             }
             trajectory.Returns.Add(discountedReturns);
         }
-    }
-    
-    
-    
-    // TESTING FUNCTIONS // 
-    
-    private class TestStep
-    {
-        public Matrix inputs;
-        public float result;
-    }
-    
-    // trains the critic neural network on test batches (approximate summing 5 numbers)
-    // used to tweak hyper-parameters and check that the neural network is working
-    public void TestTrain()
-    {
-        Random random = new Random();
-        float totalLoss = 0;
-        
-        for (int i = 0; i < Epochs; i++)
-        {
-            List<TestStep> batch = new List<TestStep>();
-            for (int j = 0; j < BatchSize; j++)
-            {
-                TestStep step = new TestStep();
-                step.inputs = Matrix.FromValues(new float[]
-                {
-                    random.Next(0, 100), random.Next(0, 10), random.Next(0, 10), random.Next(0, 10), random.Next(0, 10)
-                });
-                step.result = step.inputs.Sum();
-                
-                batch.Add(step);
-            }
-            
-            TestTrainCritic(batch, out float loss);
-            totalLoss += Math.Abs(loss);
-        }
-        Console.WriteLine($"Total overall loss: {totalLoss}.");
-    }
-
-    private void TestTrainCritic(List<TestStep> batch, out float totalLoss)
-    {
-        _criticNetwork.Zero();
-
-        totalLoss = 0;
-        for (int i = 0; i < BatchSize; i++)
-        {
-            float valueEstimate = GetValueEstimate(batch[i].inputs);
-            float criticLoss = -2 * (batch[i].result - valueEstimate);
-            
-            Console.WriteLine($"Guessed: {valueEstimate}, actual: {batch[i].result}");
-            criticLoss /= BatchSize;
-            totalLoss += criticLoss;
-            _criticNetwork.FeedBack(Matrix.FromValues(new float[][] { new float[] { criticLoss } }));
-        }
-        Console.WriteLine($"Total Loss: {totalLoss}");
-        _criticNetwork.Optimise();
     }
 }
